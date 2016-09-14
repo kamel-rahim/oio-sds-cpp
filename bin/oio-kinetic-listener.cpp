4f04675203891f10c6fd036a3acc7be8cb25af1f
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2016-09-14 10:41:31 +0200

    oio-kinetic-listener: new model with 1 coro/device doing everything

--- bin/oio-kinetic-listener.cpp
+++ bin/oio-kinetic-listener.cpp
@@ -26,7 +26,7 @@
 #include "oio/kinetic/coro/client/CoroutineClientFactory.h"
 
 #ifndef PERIOD_REG
-#define PERIOD_REG          4000
+#define PERIOD_REG          5000
 #endif
 
 #ifndef KINETIC_MCAST_PORT
@@ -53,19 +53,7 @@ using oio::kinetic::client::Sync;
 using oio::kinetic::rpc::Exchange;
 using oio::kinetic::rpc::GetLog;
 
-struct oio_srv_stats {
-    oio_srv_stats(double c, double i, double s, double t) : cpu{c}, io{i},
-                                                            space{s}, temp{t} {
-    }
-
-    oio_srv_stats() : cpu{0}, io{0}, space{0}, temp{0} {
-    }
-
-    double cpu, io, space, temp;
-};
-
 std::map<std::string, std::string> REGISTRATIONS;
-std::map<std::string, oio_srv_stats> STATS;
 
 std::string nsname;
 std::string url_proxy;
@@ -74,6 +62,164 @@ std::string srvtype{OIO_KINE_DEFAULT_SRVTYPE};
 CoroutineClientFactory factory;
 
 static volatile bool flag_running{true};
+
+struct stat_s {
+    double cpu, io, space, temp;
+};
+
+static void
+proxy_register(const std::string &id, const std::string &url, const stat_s st) {
+
+    // then forward them to the proxy
+    std::shared_ptr<net::Socket> client(new net::MillSocket);
+    client->connect(url_proxy);
+
+    rapidjson::StringBuffer buf;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buf);
+
+    LOG(INFO) << "Registering " << id << " url=" << url;
+
+    // Pack the REGISTRATIONS
+    writer.StartObject();
+    writer.Key("ns");
+    writer.String(nsname.c_str());
+    writer.Key("type");
+    writer.String(srvtype.c_str());
+    writer.Key("id");
+    writer.String(id.c_str());
+    writer.Key("addr");
+    writer.String(url.c_str());
+    writer.Key("tags");
+    writer.StartObject();
+
+    writer.Key("tag.up");
+    writer.Bool(true);
+    writer.Key("stat.cpu");
+    writer.Double(st.cpu);
+    writer.Key("stat.io");
+    writer.Double(st.io);
+    writer.Key("stat.space");
+    writer.Double(st.space);
+    writer.Key("stat.temp");
+    writer.Double(st.temp);
+
+    writer.EndObject();
+    writer.Key("score");
+    writer.Int(-2);
+    writer.EndObject();
+
+    std::string out;
+    http::Call req(client);
+    req.Method("POST")
+            .Selector("/v3.0/" + nsname + "/conscience/register")
+            .Field("Host", "proxy")
+            .Field("Connection", "keep-alive")
+            .Field("Content-Type", "application/json");
+
+    auto rc = req.Run(buf.GetString(), out);
+    LOG_IF(WARNING, rc != http::Code::OK) << "Registration failed";
+
+    client->close();
+}
+
+/**
+ * Collect the stats aboud 'id' and push the registration to the proxy
+ * @param id the UUID of the service
+ * @param url the network endpoint of the service
+ */
+static void monitoring_round(const std::string id, const std::string url) {
+    GetLog op;
+    do {
+        auto client = factory.Get(url);
+        auto sync = client->Start(&op);
+        sync->Wait();
+        if (!op.Ok()) {
+            LOG(INFO) << "Stat [" << id << "] FAILED";
+            return;
+        }
+    } while (0);
+    proxy_register(id, url, {op.getCpu(), op.getIo(),
+                             op.getSpace(), op.getTemp()});
+    DLOG(INFO) << "Stat [" << id << "] done";
+}
+
+static coroutine void monitoring_loop(const std::string &id0) {
+    // Immediately make a copy of the input ID, so that the original string
+    // may be freed but it exists a copy in the ccurrent stack frame.
+    std::string id(id0);
+    DLOG(INFO) << "Monitoring [" << id << "]";
+    yield();
+
+    int64_t next_check = mill_now();
+    while (flag_running) {
+        monitoring_round(id, REGISTRATIONS[id]);
+        next_check += PERIOD_REG;
+        if (mill_now() < next_check)
+            msleep(next_check);
+    }
+}
+
+static void manage_message(rapidjson::Document &msg) {
+
+    // Schema validation
+    if (!msg.HasMember("network_interfaces") || !ITF.IsArray() ||
+        ITF.Size() <= 0)
+        return;
+    if (!ITF1.HasMember("ipv4_addr") || !IPV4.IsString())
+        return;
+    if (!msg.HasMember("port") || !PORT.IsInt())
+        return;
+    if (!msg.HasMember("world_wide_name") || !WWN.IsString())
+        return;
+
+    std::stringstream ss;
+    ss << IPV4.GetString() << ":" << PORT.GetInt();
+    const auto uuid = WWN.GetString();
+    const auto url = ss.str();
+
+    // We have a full registration, save or update it
+    auto it = REGISTRATIONS.find(uuid);
+    if (it == REGISTRATIONS.end()) {
+        REGISTRATIONS[uuid] = url;
+        mill_go(monitoring_loop(uuid));
+    } else if (it->second != ss.str()) {
+        REGISTRATIONS[uuid] = url;
+    }
+}
+
+static coroutine void consumer(int fd, chan done) {
+    struct sockaddr_in sa;
+    std::array<uint8_t, 4096> buffer;
+    while (flag_running) {
+        memset(&sa, 0, sizeof(sa));
+        socklen_t salen = sizeof(sa);
+        auto rc = ::recvfrom(fd, buffer.data(), buffer.size() - 1, 0,
+                             (struct sockaddr *) &sa, &salen);
+        if (rc < 0) {
+            if (errno == EINTR)
+                continue;
+            else if (errno == EAGAIN) {
+                int evt = fdwait(fd, FDW_IN, mill_now() + 5000);
+                if (evt & FDW_ERR)
+                    break;
+                continue;
+            } else {
+                break;
+            }
+        }
+        if (rc == 0)
+            break;
+
+        buffer[rc] = 0;
+        rapidjson::Document msg;
+        if (msg.Parse<0>((const char *) buffer.data()).HasParseError()) {
+            LOG_EVERY_N(ERROR, 512) << "Invalid Frame";
+        } else {
+            manage_message(msg);
+        }
+    }
+    chs(done, bool, true);
+}
 
 static int make_kinetic_socket(void) {
     int fd = ::socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);
@@ -110,180 +256,11 @@ static int make_kinetic_socket(void) {
     return fd;
 }
 
-/**
- * Returns a map of <key,value> tags for the service identified by 'url'
- * @param id the network endpoint of the service
- * @param url the network endpoint of the service
- * @return the tags for the service
- */
-static NOINLINE bool
-qualify(const std::string id, const std::string url) {
-    GetLog op;
-    auto client = factory.Get(url);
-    auto sync = client->Start(&op);
-    sync->Wait();
-    if (!op.Ok()) {
-        LOG(INFO) << "Failed to stat " << url;
-        return false;
-    } else {
-        STATS[id] = oio_srv_stats(op.getCpu(), op.getIo(), op.getSpace(),
-                                  op.getTemp());
-        DLOG(INFO) << "Stat " << id << "/" << url << " -> ok";
-        return true;
-    }
-}
-
-static NOINLINE void push() {
-
-    if (REGISTRATIONS.empty()) {
-        DLOG(INFO) << "No registration this turn!";
-        return;
-    }
-
-    DLOG(INFO) << "Sending " << REGISTRATIONS.size() << " registrations";
-
-    std::map<std::string, std::string> registrations;
-    registrations.swap(REGISTRATIONS);
-
-    // then forward them to the proxy
-    std::shared_ptr<net::Socket> client(new net::MillSocket);
-    client->connect(url_proxy);
-
-    for (const auto reg: registrations) {
-        rapidjson::StringBuffer buf;
-        rapidjson::Writer<rapidjson::StringBuffer> writer(buf);
-
-        LOG(INFO) << "Registering " << reg.first << " url=" << reg.second;
-        auto qualified = qualify(reg.first, reg.second);
-
-        // Pack the REGISTRATIONS
-        writer.StartObject();
-        writer.Key("ns");
-        writer.String(nsname.c_str());
-        writer.Key("type");
-        writer.String(srvtype.c_str());
-        writer.Key("id");
-        writer.String(reg.first.c_str());
-        writer.Key("addr");
-        writer.String(reg.second.c_str());
-        writer.Key("tags");
-        writer.StartObject();
-        writer.Key("tag.up");
-        if (!qualified) {
-            writer.Bool(false);
-        } else {
-            writer.Bool(true);
-            const auto st = STATS[reg.first];
-            writer.Key("stat.cpu");
-            writer.Double(st.cpu);
-            writer.Key("stat.io");
-            writer.Double(st.io);
-            writer.Key("stat.space");
-            writer.Double(st.space);
-            writer.Key("stat.temp");
-            writer.Double(st.temp);
-        }
-        writer.EndObject();
-        writer.Key("score");
-        writer.Int(-2);
-        writer.EndObject();
-
-        std::string out;
-        http::Call req(client);
-        req.Method("POST")
-                .Selector("/v3.0/" + nsname + "/conscience/register")
-                .Field("Host", "proxy")
-                .Field("Connection", "keep-alive")
-                .Field("Content-Type", "application/json");
-
-        auto rc = req.Run(buf.GetString(), out);
-        LOG_IF(WARNING, rc != http::Code::OK) << "Registration failed";
-    }
-    registrations.clear();
-
-    client->close();
-}
-
-static NOINLINE void push_and_set_deadline(int64_t &next_dl) {
-    push();
-    next_dl = mill_now() + PERIOD_REG;
-}
-
-static coroutine void registrar(chan done, chan wake) {
-    int64_t next_dl = mill_now();
-    while (flag_running) {
-        mill_choose {
-                mill_in(wake, int, s):
-                    if (REGISTRATIONS.size() >= 256)
-                        push_and_set_deadline(next_dl);
-                    (void) s;
-                mill_deadline(next_dl):
-                    push_and_set_deadline(next_dl);
-            mill_end
-        }
-    }
-    chs(done, bool, true);
-}
-
-static void manage_message(chan wake, rapidjson::Document &msg) {
-
-    // Schema validation
-    if (!msg.HasMember("network_interfaces") || !ITF.IsArray() ||
-        ITF.Size() <= 0)
-        return;
-    if (!ITF1.HasMember("ipv4_addr") || !IPV4.IsString())
-        return;
-    if (!msg.HasMember("port") || !PORT.IsInt())
-        return;
-    if (!msg.HasMember("world_wide_name") || !WWN.IsString())
-        return;
-
-    // We have a full registration, save it
-    std::stringstream ss;
-    ss << IPV4.GetString() << ":" << PORT.GetInt();
-    REGISTRATIONS[WWN.GetString()] = ss.str();
-
-    // Ping the sender sender coroutine
-    chs(wake, int, 1);
-}
-
-static coroutine void consumer(int fd, chan done, chan wake) {
-    struct sockaddr_in sa;
-    std::array<uint8_t, 4096> buffer;
-    while (flag_running) {
-        memset(&sa, 0, sizeof(sa));
-        socklen_t salen = sizeof(sa);
-        auto rc = ::recvfrom(fd, buffer.data(), buffer.size() - 1, 0,
-                             (struct sockaddr *) &sa, &salen);
-        if (rc < 0) {
-            if (errno == EINTR)
-                continue;
-            else if (errno == EAGAIN) {
-                int evt = fdwait(fd, FDW_IN, mill_now() + 5000);
-                if (evt & FDW_ERR)
-                    break;
-                continue;
-            } else {
-                break;
-            }
-        }
-        if (rc == 0)
-            break;
-
-        buffer[rc] = 0;
-        rapidjson::Document msg;
-        if (msg.Parse<0>((const char *) buffer.data()).HasParseError()) {
-            LOG_EVERY_N(ERROR, 512) << "Invalid Frame";
-        } else {
-            manage_message(wake, msg);
-        }
-    }
-    chs(done, bool, true);
-}
-
 int main(int argc UNUSED, char **argv) {
     google::InitGoogleLogging(argv[0]);
     FLAGS_logtostderr = true;
+    FLAGS_minloglevel = google::INFO;
+    FLAGS_colorlogtostderr = true;
 
     if (argc != 3) {
         LOG(ERROR) << "Usage: " << argv[0] << " NS URL";
@@ -300,12 +277,8 @@ int main(int argc UNUSED, char **argv) {
     DLOG(INFO) << "mcast socket fd=" << fd;
 
     chan done = chmake(bool, 2);
-    chan wake = chmake(int, 0);
-    mill_go(consumer(fd, done, wake));
-    mill_go(registrar(done, wake));
+    mill_go(consumer(fd, done));
     chr(done, bool);
-    chr(done, bool);
-    chclose(wake);
     chclose(done);
 
     ::fdclean(fd);
